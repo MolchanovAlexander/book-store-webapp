9bd3f3266e41177b34cd773b096f235b7355b68b
MolchanovAlexander <rybkolhoz@gmail.com>
2024-04-18 16:10:40 +0300

    initial commit

---
+++ src/main/java/com/example/bookstorewebapp/BookStoreWebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookstorewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookStoreWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreWebappApplication.class, args);
+    }
+
+}
