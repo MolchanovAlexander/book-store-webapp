e67d60e4dc33085b0472f5bb6414feb1740c1a3e
MolchanovAlexander <rybkolhoz@gmail.com>
2024-04-18 16:25:08 +0300

    start commit

---
+++ src/main/java/com/example/bookstorewebapp/model/Book.java
@@ -0,0 +1,18 @@
+package com.example.bookstorewebapp.model;
+
+import java.math.BigDecimal;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private BigDecimal price;
+}
