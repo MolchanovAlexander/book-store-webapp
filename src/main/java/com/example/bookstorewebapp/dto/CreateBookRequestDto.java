5575fcbd327aed1c68494f1d122c2654d4598341
MolchanovAlexander <rybkolhoz@gmail.com>
2024-04-30 12:16:38 +0300

    task 5 minor changes

--- src/main/java/com/example/bookstorewebapp/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/bookstorewebapp/dto/CreateBookRequestDto.java
@@ -3,6 +3,7 @@ package com.example.bookstorewebapp.dto;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
@@ -16,7 +17,7 @@ public class CreateBookRequestDto {
     @Size(min = 2, max = 255)
     private String author;
     @NotBlank
-    @Size(min = 10, max = 17)
+    @Pattern(regexp = "^\\d{13}$", message = "The ISBN must contain 13 digits")
     private String isbn;
     @NotNull
     @Min(0)
